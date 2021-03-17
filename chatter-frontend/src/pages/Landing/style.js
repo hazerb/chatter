7872cfd183bb6d8a3bb696e9b3b37fec7313349e
hazerb <60961048+hazerb@users.noreply.github.com>
2021-03-17 21:46:53 +0300

    Delete chatter-frontend directory

--- chatter-frontend/src/pages/Landing/style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Content = styled.div`
-  background-position: center top;
-  background-image: url(logo.jpg);
-  background-size: 1100px 900px;
-  height: 100vh;
-`;
